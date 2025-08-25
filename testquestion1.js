ce7c600225e6a2762d07ebf3ca3f56ec60e579a1
Computhink-CS136 <cs136@computhink.com.sg>
2025-08-25 10:24:17 +0000

    tests

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,13 @@
-
+function setup() {
+    createCanvas(600, 400);
+    background(0)
+}
+function draw() {
+    background(0);
+    fill("black");
+    stroke("red");
+    strokeWeight(10);
+    circle(100, 200, 100);
+    triangle(220, 250, 340, 250, 280, 150);
+    rect(450, 150, 100, 100);
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,19 @@
 
+
+function setup() {
+    createCanvas(600, 600);
+    background(220)
+}
+function draw() {
+    background(220);
+let xpos = 50
+let ypos = 50
+let color = 1
+for(let i = 0; i < 11; i++){
+    fill(color)
+    color = color + 25
+    circle(xpos, ypos, 50);
+    xpos = xpos + 50;
+    ypos = ypos + 50;
+}
+}
